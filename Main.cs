c733fc01af0c2e254aa5647c65f3c08ec9e5df83
Philip Løventoft <philip@lovenit.dk>
2022-06-14 12:27:57 +0200

    Initial commit.

---
+++ Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReactiveUI;
+
+namespace ReactiveUIWinformsBug
+{
+    public partial class Main : Form, IViewFor<Viewmodel>
+    {
+        private Viewmodel _vm;
+
+        object IViewFor.ViewModel
+        {
+            get => _vm;
+            set => _vm = (Viewmodel)value;
+        }
+
+        Viewmodel IViewFor<Viewmodel>.ViewModel
+        {
+            get => _vm;
+            set => _vm = value;
+        }
+
+        public Main(Viewmodel vm)
+        {
+            _vm = vm;
+            InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                d(this.OneWayBind(_vm, v => v.Button1Enabled, form => form.button1.Enabled));
+                d(this.BindCommand(_vm, v => v.DisableButton1, form => form.button1));
+
+
+                d(this.OneWayBind(_vm, v => v.Button2Enabled, form => form.button2.Enabled));
+                d(this.BindCommand(_vm, v => v.DisableButton2, form => form.button2));
+            });
+
+
+        }
+    }
+}
---
+++ Viewmodel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReactiveUI;
+
+namespace ReactiveUIWinformsBug
+{
+    public class Viewmodel : ReactiveObject
+    {
+        private bool _button1Enabled = true;
+        public bool Button1Enabled
+        {
+            get => _button1Enabled;
+            set => this.RaiseAndSetIfChanged(ref _button1Enabled, value);
+        }
+        private bool _button2Enabled = true;
+        public bool Button2Enabled
+        {
+            get => _button2Enabled;
+            set => this.RaiseAndSetIfChanged(ref _button2Enabled, value);
+        }
+
+
+        public ReactiveCommand<Unit, Unit> DisableButton1 { get; private set; }
+
+        public ReactiveCommand<Unit, Unit> DisableButton2 { get; private set; }
+
+
+        public Viewmodel()
+        {
+            DisableButton1 = ReactiveCommand.Create(() =>
+            {
+                Observable.Start(() =>
+                {
+                    Button1Enabled = false;
+                }, RxApp.MainThreadScheduler);
+            });
+            DisableButton2 = ReactiveCommand.Create(() =>
+            {
+                Observable.Start(() =>
+                {
+                    Application.DoEvents();
+                    Button2Enabled = false;
+                }, RxApp.MainThreadScheduler);
+            });
+        }
+    }
+}
