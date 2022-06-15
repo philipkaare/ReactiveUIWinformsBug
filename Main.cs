118b8a6029be582d151387c0b7303523a0fdc50a
Chris Pulman <chris.pulman@yahoo.com>
2022-06-15 14:43:39 +0100

    Added colour change

--- Main.cs
+++ Main.cs
@@ -35,10 +35,10 @@ namespace ReactiveUIWinformsBug
 
             this.WhenActivated(d =>
             {
-                //d(this.OneWayBind(_vm, v => v.Button1Enabled, form => form.button1.Enabled));
+                d(this.OneWayBind(_vm, v => v.Button1Colour, form => form.button1.BackColor));
                 d(this.BindCommand(_vm, v => v.DisableButton1, form => form.button1));
 
-
+                d(this.OneWayBind(_vm, v => v.Button2Colour, form => form.button2.BackColor));
                 d(this.OneWayBind(_vm, v => v.Button2Enabled, form => form.button2.Enabled));
                 d(this.BindCommand(_vm, v => v.DisableButton2, form => form.button2));
             });
--- Viewmodel.cs
+++ Viewmodel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Windows.Forms;
@@ -21,6 +22,19 @@ namespace ReactiveUIWinformsBug
             set => this.RaiseAndSetIfChanged(ref _button2Enabled, value);
         }
 
+        private Color _button1Colour = Color.FromKnownColor(KnownColor.Control);
+        public Color Button1Colour
+        {
+            get => _button1Colour;
+            set => this.RaiseAndSetIfChanged(ref _button1Colour, value);
+        }
+
+        private Color _button2Colour = Color.FromKnownColor(KnownColor.Control);
+        public Color Button2Colour
+        {
+            get => _button2Colour;
+            set => this.RaiseAndSetIfChanged(ref _button2Colour, value);
+        }
 
         public ReactiveCommand<Unit, Unit> DisableButton1 { get; private set; }
 
@@ -31,11 +45,17 @@ namespace ReactiveUIWinformsBug
         {
             // Create command with CanExecute condition
             var btn1Enabled = this.WhenAnyValue(x => x.Button1Enabled);
+            var newColour = 0;
             DisableButton1 = ReactiveCommand.Create(() =>
             {
                 // Disable the command. This automatically disables the UI via the command binding
-                // NOTE: execution happens on the taskpool thread.
+                // NOTE: execution happens on the taskpool thread, result returns on the UI thread.
                 Button1Enabled = false;
+
+                // Change button colour
+                newColour += 50;
+                Button1Colour = Color.FromArgb(255, newColour, 0);
+                if (newColour > 255) newColour = 0;
             }, btn1Enabled);
 
             DisableButton2 = ReactiveCommand.Create(() =>
@@ -46,6 +66,7 @@ namespace ReactiveUIWinformsBug
                     Application.DoEvents();
                     // Set the Enabled state of the Button to false.
                     Button2Enabled = false;
+                    Button2Colour = Color.FromArgb(255, newColour, 0);
                 }, RxApp.MainThreadScheduler);
             });
 
